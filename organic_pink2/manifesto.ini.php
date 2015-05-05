925355eab1171f26381864882f05beb5a184d19b
mambax7 <mambax7@gmail.com>
2015-05-05 09:23:00 -0400

    Original files

---
+++ organic_pink2/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="BCOOL THEME ORGANIC_PINK2"
+Depends=Legacy_RenderSystem
+Url="http://2bcool.net/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=M.Taka BCOOL Inc.
+ScreenShot="screenshot.png"
+Description="1 or 2 or 3columns, variable width type simple theme."
+W3C=
+
+License="Creative Commons License(Attribution-NonCommercial-ShareAlike 2.1 Japan)"
+
+*/
+?>
